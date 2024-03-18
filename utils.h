04ff01b0e3b9aae91dc31a4d5f27836b6734c3a8
abhradipg <abhradip.ghosh9@gmail.com>
2024-03-18 19:17:15 +0530

    debug

--- utils.h
+++ utils.h
@@ -129,9 +129,9 @@ int run_testcase3()
 {
         int *buff,ret;
 
-        buff =  mmap((void *)0x7ff7ca71e000, sizeof(int) * num_elements, PROT_READ | PROT_WRITE ,MAP_ANONYMOUS, 0, 0);
+        buff =  mmap((void *)0x7ff7ca71e000, sizeof(int) * num_elements, PROT_READ | PROT_WRITE ,MAP_ANON| MAP_SHARED, -1, 0);
         if (buff == MAP_FAILED){
-               printf("failed unmap\n");
+               printf("failed mmap\n");
                return 0;
         }
         assert(buff != NULL);
